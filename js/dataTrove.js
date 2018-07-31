7ad19acf798e3e305ded27518ec0df86f6ab2fc7
Ben Carroll <ben.andrew.carroll@gmail.com>
2018-07-31 12:31:49 +1000

    The thing that actually needs to be there for offline to work. Duh

---
+++ js/dataTrove.js
@@ -0,0 +1,296 @@
+// I know, this is terrible
+
+// Had to come up with something ASAP because working with CORS is impossible
+
+dataTrove = {BHP: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "BHP.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "32.8150",
+            "2. high": "32.8150",
+            "3. low": "32.7600",
+            "4. close": "32.7800",
+            "5. volume": "55103"
+        },
+        "2018-06-18 01:59:00": {
+            "1. open": "32.8100",
+            "2. high": "32.8200",
+            "3. low": "32.8000",
+            "4. close": "32.8150",
+            "5. volume": "11714"
+        },
+        "2018-06-18 01:58:00": {
+            "1. open": "32.8200",
+            "2. high": "32.8300",
+            "3. low": "32.8100",
+            "4. close": "32.8100",
+            "5. volume": "21234"
+        }
+}},
+
+
+ANZ: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "ANZ.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "27.0300",
+            "2. high": "27.0400",
+            "3. low": "27.0100",
+            "4. close": "27.0100",
+            "5. volume": "16984"
+        },
+        "2018-06-18 01:59:00": {
+            "1. open": "27.0100",
+            "2. high": "27.0300",
+            "3. low": "27.0100",
+            "4. close": "27.0300",
+            "5. volume": "30459"
+        }
+
+
+
+}},
+
+BKL: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "BKL.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "137.0700",
+            "2. high": "137.0700",
+            "3. low": "136.8000",
+            "4. close": "136.9000",
+            "5. volume": "249"
+        },
+        "2018-06-18 01:58:00": {
+            "1. open": "137.1500",
+            "2. high": "137.1500",
+            "3. low": "137.0200",
+            "4. close": "137.1000",
+            "5. volume": "123"
+        }
+}},
+
+CBA: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "CBA.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "69.6100",
+            "2. high": "69.6500",
+            "3. low": "69.6000",
+            "4. close": "69.6500",
+            "5. volume": "3374"
+        },
+        "2018-06-18 01:59:00": {
+            "1. open": "69.6000",
+            "2. high": "69.6300",
+            "3. low": "69.5700",
+            "4. close": "69.6300",
+            "5. volume": "12100"
+        }
+      }},
+
+NAB: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "NAB.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "26.5200",
+            "2. high": "26.5300",
+            "3. low": "26.5000",
+            "4. close": "26.5200",
+            "5. volume": "23536"
+        },
+        "2018-06-18 01:59:00": {
+            "1. open": "26.5100",
+            "2. high": "26.5200",
+            "3. low": "26.5000",
+            "4. close": "26.5200",
+            "5. volume": "22659"
+        }
+      }},
+QAN: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "QAN.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "6.5100",
+            "2. high": "6.5100",
+            "3. low": "6.5000",
+            "4. close": "6.5100",
+            "5. volume": "10771"
+        },
+        "2018-06-18 01:59:00": {
+            "1. open": "6.5000",
+            "2. high": "6.5100",
+            "3. low": "6.5000",
+            "4. close": "6.5050",
+            "5. volume": "5862"
+        }}},
+
+RIO: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "RIO.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "84.0500",
+            "2. high": "84.0600",
+            "3. low": "83.9500",
+            "4. close": "83.9600",
+            "5. volume": "4006"
+        },
+        "2018-06-18 01:59:00": {
+            "1. open": "84.0000",
+            "2. high": "84.0600",
+            "3. low": "84.0000",
+            "4. close": "84.0400",
+            "5. volume": "3791"
+        }}},
+
+TLS: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "TLS.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "2.8650",
+            "2. high": "2.8700",
+            "3. low": "2.8600",
+            "4. close": "2.8700",
+            "5. volume": "115162"
+        },
+        "2018-06-18 01:59:00": {
+            "1. open": "2.8700",
+            "2. high": "2.8700",
+            "3. low": "2.8650",
+            "4. close": "2.8700",
+            "5. volume": "27032"
+        }}},
+
+WBC: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "WBC.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "28.1200",
+            "2. high": "28.1300",
+            "3. low": "28.1100",
+            "4. close": "28.1300",
+            "5. volume": "10820"
+        },
+        "2018-06-18 01:59:00": {
+            "1. open": "28.1200",
+            "2. high": "28.1300",
+            "3. low": "28.1100",
+            "4. close": "28.1250",
+            "5. volume": "10074"
+        }}},
+
+WES: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "WES.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "47.8450",
+            "2. high": "47.8500",
+            "3. low": "47.8100",
+            "4. close": "47.8300",
+            "5. volume": "8204"
+        },
+        "2018-06-18 01:59:00": {
+            "1. open": "47.8250",
+            "2. high": "47.8500",
+            "3. low": "47.8250",
+            "4. close": "47.8500",
+            "5. volume": "2499"
+        }}},
+
+WOW: {
+    "Meta Data": {
+        "1. Information": "Intraday (1min) prices and volumes",
+        "2. Symbol": "WOW.AX",
+        "3. Last Refreshed": "2018-06-18 02:00:00",
+        "4. Interval": "1min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (1min)": {
+        "2018-06-18 02:00:00": {
+            "1. open": "29.3600",
+            "2. high": "29.3700",
+            "3. low": "29.3500",
+            "4. close": "29.3600",
+            "5. volume": "14354"
+        },
+        "2018-06-18 01:59:00": {
+            "1. open": "29.3700",
+            "2. high": "29.3700",
+            "3. low": "29.3600",
+            "4. close": "29.3600",
+            "5. volume": "7848"
+        }}}
+
+
+}
